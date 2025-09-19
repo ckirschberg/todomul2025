90b1bfdb08b0fb421a924c4b857d928546e3f9e7
Christian Kirschberg <ckirschberg@gmail.com>
2025-09-19 12:27:22 +0200

    forms

--- src/App.js
+++ src/App.js
@@ -9,16 +9,19 @@ import { useState } from 'react';
 // }
 // add(5,2);
 // add(7,2);
+  const hardCodedTodos = [
+        new Todo(1, 'Køb ind', false, new Date(2025,0,1,12,20)),
+        new Todo(2, 'Gå tur med hunden', true, new Date(2025,0,1,12,20)),
+        new Todo(3, 'Gør Persian endnu sejere', false, new Date(2025,0,1,12,20)),
+    ]
+
 
 function App() {
   const [ happy, setHappy ] = useState(true); // false er startværdien
-
+  const [ todos, setTodos ] = useState(hardCodedTodos);
+  const [ title, setTitle ] = useState("")
   // Vi 'leger' at vi har hentet disse data fra serveren og databasen.
-  const todos = [
-        new Todo(1, 'Køb ind', false, new Date(2025,0,1,12,20)),
-        new Todo(2, 'Gå tur med hunden', true, new Date(2025,0,1,12,20)),
-        new Todo(3, 'Gør Perian endnu sejere', false, new Date(2025,0,1,12,20)),
-    ]
+
 
     function switchBoolean() {
       // let newHappy = false;
@@ -31,18 +34,46 @@ function App() {
       setHappy(!happy)
     }
 
+    function addTodo(e) {
+      e.preventDefault()
+      // Must know title
+
+      if (title !== "") {
+        const newTodoItem = new Todo(todos.length+1, title, false, new Date())
+        
+        //[...todos ]: This will create a new array and spread the existing objects into it.
+
+        setTodos([...todos, newTodoItem ]);
+      }
+      else {
+        alert("Du skal da udfylde først for søren da")
+      }
+      // Tilføj newTodoItem til min todo list 
+    }
+    function handleTitleChange(e) {
+      console.log(e.target.value);
+      
+      setTitle(e.target.value);
+    }
+
     // jsx
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         
-        <button onClick={switchBoolean}>Are you happy?</button>
+        <button onClick={switchBoolean}>Are you happy? NO!!!</button>
         <p>
           {happy.toString()}
         </p>
 
 
+        <h2>Add new todo</h2>
+        <form onSubmit={addTodo}>
+          <input type="text" placeholder="Title" value={title} onChange={handleTitleChange}/>
+          
+          <button type="submit">Add todo</button>
+        </form>
         <TodoList todoItems={todos}/>
 
         <a
